25e8231aee90ed3a9f8d90622816ffa459594799
Mateusz Wojciechowski <120648222+Mateusz-Wojciechowski@users.noreply.github.com>
2024-06-03 13:40:29 +0200

    modified, cache added

--- Lab10_JSkryptowe/load_data.py
+++ Lab10_JSkryptowe/load_data.py
@@ -6,22 +6,38 @@ from sqlalchemy.orm import sessionmaker
 from create_database import Station, Base
 
 
-def check_station_id(station_name, session):
+def initialize_cache(session):
+    stations = session.query(Station).all()
+    return {station.station_name: station.station_id for station in stations}
+
+
+def get_or_create_station(station_name, station_cache, session):
+    if station_name in station_cache:
+        return station_cache[station_name]
+
     station = session.query(Station).filter_by(station_name=station_name).first()
+
     if station is None:
         station = Station(station_name=station_name)
         session.add(station)
         session.commit()
         session.refresh(station)
-    return station.station_id
+        station_cache[station_name] = station.station_id
+    else:
+        station_cache[station_name] = station
+
+    return station_cache[station_name]
+
 
 
 def add_rental(data_path, session):
     data = pd.read_csv(data_path)
     rentals_data = []
+
+    station_cache = initialize_cache(session)
     for _, row in data.iterrows():
-        rental_station_id = check_station_id(row['Stacja wynajmu'], session)
-        return_station_id = check_station_id(row['Stacja zwrotu'], session)
+        rental_station_id = get_or_create_station(row['Stacja wynajmu'], station_cache, session)
+        return_station_id = get_or_create_station(row['Stacja zwrotu'], station_cache, session)
         rentals_data.append({
             'rental_id': row['UID wynajmu'],
             'bike_number': row['Numer roweru'],
